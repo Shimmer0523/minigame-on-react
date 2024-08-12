1c1c0b5578995837a18dcd4520d40689e1ba6dfd
Shimmer0523 <AndroCoffee@gmail.com>
2024-08-12 05:13:19 +0000

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,35 @@
-import "./App.css";
-import { TextStyle } from "pixi.js";
-import { Stage, Container, Text } from "@pixi/react";
-import useInput from "./components/useInput";
-import Player from "./components/Player";
-import { useCallback, useRef } from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const stageRef = useRef(null);
-  const containerRef = useRef(null);
-  const inputHandler = useInput(
-    [
-      "w",
-      "a",
-      "s",
-      "d",
-      "ArrowUp",
-      "ArrowLeft",
-      "ArrowDown",
-      "ArrowRight",
-      "Escape",
-      "Tab",
-      "Space",
-    ],
-    [0]
-  );
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="app">
-      <header className="header">
-        <h1>dev</h1>
-      </header>
-      <main className="main">
-        <div className="overlay">
-          <div className="side-bar">
-            <p>Hello, world.</p>
-            <span>My life is going.</span>
-            <br />
-            <button>???</button>
-            <button>!!!</button>
-          </div>
-        </div>
-        <Stage
-          className="stage"
-          width={640}
-          height={480}
-          options={{ background: "#222222" }}
-          ref={stageRef}
-        >
-          <Container x={0} y={0} ref={containerRef}>
-            <Text
-              text={`${inputHandler.getMousePosiotion().x}`}
-              anchor={0}
-              x={0}
-              y={0}
-              filters={[]}
-              style={
-                new TextStyle({
-                  align: "center",
-                  fill: "#fff235",
-                  fontSize: 50,
-                  letterSpacing: 2,
-                  dropShadow: true,
-                  dropShadowColor: "0xE72264",
-                  dropShadowDistance: 6,
-                })
-              }
-            ></Text>
-            <Player inputHandler={inputHandler} />
-          </Container>
-        </Stage>
-      </main>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/minigame-on-react/",
-  build: {
-    outDir: "docs",
-  },
-  server: {
-    host: "0.0.0.0",
-  },
-});
+server: { host: true, },
+  base: '/minigame-on-react/',
+  build: { outDir: 'docs', },
+})
